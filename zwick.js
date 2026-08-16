d4cc44ab9bc4085ab040722b015d2fc137e91dca
iki1uc <entertrainer@web.de>
2026-08-17 01:07:50 +0200

    Update zwick.js

--- zwick.js
+++ zwick.js
@@ -1,7 +1,11 @@
-function nerdCharm(name = "Tomaten-Sammler") {
-    const nerd = `🧠 Nerd-Level: mild`;
-    const charm = `✨ Charm: hoch`;
-    const rums = `💥 Rums: bereit`;
+function zwick(name = "Zwick") {
+    const zap = "⚡ ZPK";
+    const wit = "🪝 WIT";
+    const lang = "📡 LNG";
+
+    return `${name}: ${zap} | ${wit} | ${lang}`;
+}
+
 
     return `${name}, du bist offiziell: ${nerd} | ${charm} | ${rums}`;
 }
