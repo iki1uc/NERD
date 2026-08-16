a7ea3fe2ca1d5d86ccd924f9e716897cf3e0a766
iki1uc <entertrainer@web.de>
2026-08-17 01:06:41 +0200

    Create nerdCharm.js

---
+++ nerdCharm.js
@@ -0,0 +1,7 @@
+function nerdCharm(name = "Tomaten-Sammler") {
+    const nerd = "🧠 NER";
+    const charm = "✨ CHM";
+    const rums = "💥 RMS";
+
+    return `${name}: ${nerd} | ${charm} | ${rums}`;
+}
