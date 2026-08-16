30762c5e973007fa9a169e5e4eac1ad5fdec5831
iki1uc <entertrainer@web.de>
2026-08-17 01:00:49 +0200

    Create zwick.js

---
+++ zwick.js
@@ -0,0 +1,7 @@
+function nerdCharm(name = "Tomaten-Sammler") {
+    const nerd = `🧠 Nerd-Level: mild`;
+    const charm = `✨ Charm: hoch`;
+    const rums = `💥 Rums: bereit`;
+
+    return `${name}, du bist offiziell: ${nerd} | ${charm} | ${rums}`;
+}
